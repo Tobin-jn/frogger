e9b94f9e646ad54ce4ea3f954f4a52802e96420c
Tobin Nelson <tobin.j.n@gmail.com>
2018-08-16 11:28:47 -0600

    Test for Game Object

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,6 +1,11 @@
 // Game-test.js
 const { assert } = require('chai');
-const Game = require('../lib/Game');
+const Game = require('../lib/Game.js');
+const Block = require('../lib/Block.js');
+const Frog = require('../lib/Frog.js');
+const Car = require('../lib/Car.js');
+
+
 
 const ctx = {
   canvas: {
@@ -11,14 +16,65 @@ const ctx = {
 
 describe('Game', () => {
 
-  it('should instantiate a new block', () => {
-    var block = new Block();
-    assert.isObject(block);
-  })  
+  // it('should instantiate a new blocks array', () => {
+  //   var block = [];
+  //   assert.isArray(block);
+  // })  
+
+  it('should instantiate a new game', () => {
+    let game = new Game();
+    assert.isObject(game);
+  })
+  it('should create a new blocks array', () => {
+    let blocks = [];
+    assert.isArray(blocks);
+  }) 
+  it('should instantiate a new block inside the canvas', () => {
+    let block = new Block(70, 100, 10, 10, 'red', 'black')
+    assert.deepEqual(block, {
+      x: 70,
+      y: 100,
+      dx: 1,
+      dxv: 1,
+      dy: 0,
+      dyv: 1,
+      height: 10,
+      width: 10,
+      color: 'red',
+      borderColor: 'black'
+    })
+  })
+  it('should create a new cars array', () => {
+    let cars = [];
+    assert.isArray(cars);
+  }) 
+  it('should instantiate a new car inside the canvas', () => {
+    let car = new Car(70, 100, 10, 10, 'red', 'black')
+    assert.deepEqual(car, {
+      x: 70,
+      y: 100,
+      dx: 1,
+      dxv: 1,
+      dy: 0,
+      dyv: 1,
+      height: 10,
+      width: 10,
+      color: 'red',
+      borderColor: 'black'
+    })
+  })
   it('should instantiate a new frog', () => {
-    var frog = new Frog();
+    let frog = new Frog();
     assert.isObject(frog);
   })
+
+
+
+
+
+
+
+
 
   //should instantiate new car
   //should instantiate new frog
